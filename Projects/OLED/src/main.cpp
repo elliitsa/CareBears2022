0e78e0d10de44f478f4657ae8864a1dfc65273b7
JobMeulenbeld <79216060+JobMeulenbeld@users.noreply.github.com>
2022-04-18 11:31:22 +0200

    Added Example code
    Added Examples codes for most of the sensors. Try and test them to see if they work :)

---
+++ Projects/OLED/src/main.cpp
@@ -0,0 +1,165 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1327_MIDAS_128X128_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+#define SUN	0
+#define SUN_CLOUD  1
+#define CLOUD 2
+#define RAIN 3
+#define THUNDER 4
+
+void drawWeatherSymbol(u8g2_uint_t x, u8g2_uint_t y, uint8_t symbol)
+{
+  // fonts used:
+  // u8g2_font_open_iconic_embedded_6x_t
+  // u8g2_font_open_iconic_weather_6x_t
+  // encoding values, see: https://github.com/olikraus/u8g2/wiki/fntgrpiconic
+  
+  switch(symbol)
+  {
+    case SUN:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 69);	
+      break;
+    case SUN_CLOUD:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 65);	
+      break;
+    case CLOUD:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 64);	
+      break;
+    case RAIN:
+      u8g2.setFont(u8g2_font_open_iconic_weather_6x_t);
+      u8g2.drawGlyph(x, y, 67);	
+      break;
+    case THUNDER:
+      u8g2.setFont(u8g2_font_open_iconic_embedded_6x_t);
+      u8g2.drawGlyph(x, y, 67);
+      break;      
+  }
+}
+
+void drawWeather(uint8_t symbol, int degree)
+{
+  drawWeatherSymbol(0, 48, symbol);
+  u8g2.setFont(u8g2_font_logisoso32_tf);
+  u8g2.setCursor(48+3, 42);
+  u8g2.print(degree);
+  u8g2.print("°C");		// requires enableUTF8Print()
+}
+
+/*
+  Draw a string with specified pixel offset. 
+  The offset can be negative.
+  Limitation: The monochrome font with 8 pixel per glyph
+*/
+void drawScrollString(int16_t offset, const char *s)
+{
+  static char buf[36];	// should for screen with up to 256 pixel width 
+  size_t len;
+  size_t char_offset = 0;
+  u8g2_uint_t dx = 0;
+  size_t visible = 0;
+  
+
+  u8g2.setDrawColor(0);		// clear the scrolling area
+  u8g2.drawBox(0, 49, u8g2.getDisplayWidth()-1, u8g2.getDisplayHeight()-1);
+  u8g2.setDrawColor(1);		// set the color for the text
+    
+  
+  len = strlen(s);
+  if ( offset < 0 )
+  {
+    char_offset = (-offset)/8;
+    dx = offset + char_offset*8;
+    if ( char_offset >= u8g2.getDisplayWidth()/8 )
+      return;
+    visible = u8g2.getDisplayWidth()/8-char_offset+1;
+    strncpy(buf, s, visible);
+    buf[visible] = '\0';
+    u8g2.setFont(u8g2_font_8x13_mf);
+    u8g2.drawStr(char_offset*8-dx, 62, buf);
+  }
+  else
+  {
+    char_offset = offset / 8;
+    if ( char_offset >= len )
+      return;	// nothing visible
+    dx = offset - char_offset*8;
+    visible = len - char_offset;
+    if ( visible > u8g2.getDisplayWidth()/8+1 )
+      visible = u8g2.getDisplayWidth()/8+1;
+    strncpy(buf, s+char_offset, visible);
+    buf[visible] = '\0';
+    u8g2.setFont(u8g2_font_8x13_mf);
+    u8g2.drawStr(-dx, 62, buf);
+  }
+  
+}
+
+void draw(const char *s, uint8_t symbol, int degree)
+{
+  int16_t offset = -(int16_t)u8g2.getDisplayWidth();
+  int16_t len = strlen(s);
+  
+  u8g2.clearBuffer();					// clear the internal memory
+  drawWeather(symbol, degree);		// draw the icon and degree only once
+  for(;;)							// then do the scrolling
+  {
+  
+    drawScrollString(offset, s);  			// no clearBuffer required, screen will be partially cleared here
+    u8g2.sendBuffer();				// transfer internal memory to the display
+
+    delay(20);
+    offset+=10;
+    if ( offset > len*8+1 )
+      break;
+  }
+}
+
+
+void setup(void) {
+
+  /* U8g2 Project: SSD1306 Test Board */
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  
+  digitalWrite(10, 0);
+  digitalWrite(9, 0);		
+
+  /* U8g2 Project: T6963 Test Board */
+  //pinMode(18, OUTPUT);
+  //digitalWrite(18, 1);	
+
+  /* U8g2 Project: KS0108 Test Board */
+  //pinMode(16, OUTPUT);
+  //digitalWrite(16, 0);	
+
+  /* U8g2 Project: LC7981 Test Board, connect RW to GND */
+  //pinMode(17, OUTPUT);
+  //digitalWrite(17, 0);	
+
+  /* U8g2 Project: Pax Instruments Shield: Enable Backlight */
+  //pinMode(6, OUTPUT);
+  //digitalWrite(6, 0);	
+
+  u8g2.begin();  
+  u8g2.enableUTF8Print();
+}
+
+void loop(void) {
+  draw("What a beautiful day!", SUN, 27);
+  draw("The sun's come out!", SUN_CLOUD, 19);
+  draw("It's raining cats and dogs.", RAIN, 8);
+  draw("That sounds like thunder.", THUNDER, 12);
+  draw("It's stopped raining", CLOUD, 15);
+}
